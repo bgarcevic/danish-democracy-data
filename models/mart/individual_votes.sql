96d2b2efb751dde899aa622170e48281e5478114
Boris Kamber Garcevic <g82024@velliv.dk>
2023-07-17 20:54:04 +0200

    .

--- models/mart/individual_votes.sql
+++ models/mart/individual_votes.sql
@@ -6,17 +6,17 @@ individual_votes_source as (
             from=ref('stg_individual_votes'), 
             except=['file_name']
         )
-    }} 
+    }}
     from {{ ref('stg_individual_votes') }}
 ),
 
 final as (
     select
         -- surrogate keys
-        {{ dbt_utils.generate_surrogate_key(['individual_vote_id']) }} as individual_vote_sk,
+        {{ dbt_utils.generate_surrogate_key(['individual_vote_id']) }} as individual_vote_sk, --noqa: LT05
         {{ dbt_utils.generate_surrogate_key(['vote_id']) }} as vote_sk,
         {{ dbt_utils.generate_surrogate_key(['actor_id']) }} as actor_sk,
-        {{ dbt_utils.generate_surrogate_key(['individual_voting_type_id']) }} as individual_voting_type_sk,
+        {{ dbt_utils.generate_surrogate_key(['individual_voting_type_id']) }} as individual_voting_type_sk, --noqa: LT05
         -- meta
         updated_at
     from individual_votes_source
