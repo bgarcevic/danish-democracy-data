e215f76bd07b99134386103619bd6e22b716f83f
Boris Kamber Garcevic <g82024@velliv.dk>
2023-07-27 08:54:36 +0200

    Add case_type to cases

--- models/mart/dim_cases.sql
+++ models/mart/dim_cases.sql
@@ -12,8 +12,12 @@ case_step_statuses_source as (
     select * from {{ ref('stg_case_step_statuses') }}
 ),
 
-cases as (
+cases_source as (
     select * from {{ ref('stg_cases') }}
+),
+
+case_type_source as (
+    select * from {{ ref('stg_case_types') }}
 ),
 
 final as (
@@ -24,21 +28,21 @@ final as (
         ) }} as case_sk,
 
         -- attributes
-        cases.case_type_id,
-        cases.case_category_id,
-        cases.case_status_id,
-        cases.case_short_title,
-        cases.case_number,
-        cases.case_number_prefix,
-        cases.case_number_numeric,
-        cases.case_number_postfix,
-        cases.case_period_id,
-        cases.case_decision_result_code,
-        cases.case_state_budget,
-        cases.case_reason,
-        cases.case_paragraph_number,
-        cases.case_decision_date,
-        cases.case_decision,
+        case_type_source.case_type,
+        cases_source.case_category_id,
+        cases_source.case_status_id,
+        cases_source.case_short_title,
+        cases_source.case_number,
+        cases_source.case_number_prefix,
+        cases_source.case_number_numeric,
+        cases_source.case_number_postfix,
+        cases_source.case_period_id,
+        cases_source.case_decision_result_code,
+        cases_source.case_state_budget,
+        cases_source.case_reason,
+        cases_source.case_paragraph_number,
+        cases_source.case_decision_date,
+        cases_source.case_decision,
         case_step_statuses_source.case_step_status,
         case_steps_source.case_step_title,
         case_step_types_source.case_step_type,
@@ -47,14 +51,16 @@ final as (
         case_steps_source.case_step_updated_at,
         case_step_types_source.case_step_type_updated_at,
         case_step_statuses_source.case_step_status_updated_at,
-        cases.case_updated_at
+        cases_source.case_updated_at
     from case_steps_source
     left join case_step_types_source
         on case_steps_source.case_step_type_id = case_step_types_source.case_step_type_id --noqa: LT05
     left join case_step_statuses_source
         on case_steps_source.case_step_status_id = case_step_statuses_source.case_step_status_id --noqa: LT05
-    left join cases
-        on case_steps_source.case_id = cases.case_id
+    left join cases_source
+        on case_steps_source.case_id = cases_source.case_id
+    left join case_type_source
+        on cases_source.case_type_id = case_type_source.case_type_id
 )
 
 select * from final
