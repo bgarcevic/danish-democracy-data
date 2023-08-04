47a24ade9852e6ede22ff5a42ce757c391184e3a
Boris Kamber Garcevic <g82024@velliv.dk>
2023-08-04 17:09:19 +0200

    cast meetings_date as date to join on date table

--- analyses/votes.sql
+++ analyses/votes.sql
@@ -20,8 +20,17 @@ individual_votes as (
     select * from {{ ref('fct_individual_votes') }}
 ),
 
+dates as (
+    select * from {{ ref('dim_dates') }}
+),
+
 joins as (
-    select * 
+    select 
+        votes.* exclude(vote_sk), 
+        actors.* exclude(actor_sk), 
+        meetings.* exclude(meeting_sk), 
+        cases.* exclude(case_sk),
+        dates.* exclude(date_sk)
     from individual_votes
     left join votes
         on individual_votes.vote_sk = votes.vote_sk
@@ -31,6 +40,8 @@ joins as (
         on individual_votes.meeting_sk = meetings.meeting_sk
     left join cases
         on individual_votes.case_sk = cases.case_sk
+    left join dates
+        on individual_votes.date_sk = dates.date_sk
 )
 
 select * from joins
