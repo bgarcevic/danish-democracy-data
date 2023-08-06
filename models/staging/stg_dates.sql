fb587e99088239d605b53521b4ae6f7022ec076e
Boris Kamber Garcevic <g82024@velliv.dk>
2023-08-06 17:28:36 +0200

    Remove dependency from fact table in stg_dates

--- models/staging/stg_dates.sql
+++ models/staging/stg_dates.sql
@@ -1,13 +1,7 @@
-{% set sql_statement %}
-    select min(individual_votes_updated_at) from {{ ref('fct_individual_votes') }}
-{% endset %}
-
-{%- set first_data_point = dbt_utils.get_single_value(sql_statement, default="'2014-01-01'") -%}
-
 with date_spine as (
 
   {{ dbt_utils.date_spine(
-      start_date = "cast('" + first_data_point + "' as date)",
+      start_date = "cast('2014-01-01' as date)",
       datepart = "day",
       end_date = "cast(date_trunc('year', current_date + interval 1 year) as date)"
      )
