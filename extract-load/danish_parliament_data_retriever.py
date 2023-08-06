95a1e498f20fb363893a473d203da715c7f620fe
Boris Kamber Garcevic <g82024@velliv.dk>
2023-08-06 16:52:35 +0200

    Add docstring and type hint to danish_parliament_data_retriever

--- extract-load/danish_parliament_data_retriever.py
+++ extract-load/danish_parliament_data_retriever.py
@@ -6,7 +6,19 @@ import concurrent.futures
 from datetime import datetime
 
 
-def make_api_request(api_url):
+def make_api_request(api_url: str) -> object:
+    """
+    Makes a request to an API and returns the response.
+
+    Parameters:
+        api_url (str): The URL of the API to make a request to.
+
+    Returns:
+        response (requests.models.Response): The response from the API.
+
+    Raises:
+        HTTPError: If the request was unsuccessful.
+    """
     # Request data from the API
     print(f"Sending request to {api_url}...")
     response = requests.get(api_url, timeout=10)
@@ -17,7 +29,22 @@ def make_api_request(api_url):
     return response
 
 
-def extract_api_data(api_url):
+def extract_api_data(api_url: str) -> list[dict]:
+    """
+    Retrieves all data from the given API, across all pages of results.
+
+    Parameters:
+        api_url (str): The URL of the API to make a request to.
+
+    Returns:
+        all_data (List[dict]): The data retrieved from the API, as a list of dictionaries.
+
+    Raises:
+        requests.exceptions.RequestException: For issues like network failure, invalid URL, etc.
+        requests.exceptions.HTTPError: When an HTTP error occurs.
+        requests.exceptions.ConnectionError: When a network problem occurs.
+        requests.exceptions.Timeout: When a request times out.
+    """
     all_data = []
 
     # Initialize a counter for the number of items retrieved
@@ -54,7 +81,19 @@ def extract_api_data(api_url):
     return all_data
 
 
-def construct_file_path(base_file_name, file_type="json", is_timestamp_file=False):
+def construct_file_path(base_file_name: str, file_type: str = "json", is_timestamp_file: bool = False) -> str:
+    """
+    Constructs a file path based on a base file name, file type and a flag indicating if it's a timestamp file.
+    The constructed path includes the current date and is relative to a 'data/raw' directory one level up from the script's directory.
+
+    Parameters:
+        base_file_name (str): The base name for the file.
+        file_type (str): The type of the file (default is "json").
+        is_timestamp_file (bool): Flag indicating whether this is a timestamp file (default is False).
+
+    Returns:
+        file_path (str): The full path to the file.
+    """
     # Get the current date and format it as a string
     date_string = datetime.now().strftime("%Y%m%d")
     base_file_name_lower = base_file_name.lower()
@@ -81,7 +120,21 @@ def construct_file_path(base_file_name, file_type="json", is_timestamp_file=Fals
     return file_path
 
 
-def save_data(data, file_path):
+def save_data(data: any, file_path: str) -> None:
+    """
+    Saves the provided data into a file at the given path. Data is saved in JSON format.
+
+    Parameters:
+        data (Any): The data to be saved. Can be of any type that is serializable by json.dump().
+        file_path (str): The path of the file where data is to be saved.
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If there is an I/O error while writing to the file.
+        Exception: If there is an unexpected error.
+    """
     try:
         # Save the data to a file
         with open(file_path, "w", encoding='utf8') as file:
@@ -95,7 +148,17 @@ def save_data(data, file_path):
         print("Unexpected error:", sys.exc_info()[0])
 
 
-def retrieve_data(api_url, base_file_name):
+def retrieve_data(api_url: str, base_file_name: str) -> None:
+    """
+    Retrieves data from an API, saves it to a file, and saves the timestamp of the run.
+
+    Parameters:
+        api_url (str): The URL of the API to retrieve data from.
+        base_file_name (str): The base name for the files where data and timestamp will be saved.
+
+    Returns:
+        None
+    """
     # Retrieve data from the API
     all_data = extract_api_data(api_url)
     file_path = construct_file_path(base_file_name)
@@ -109,6 +172,7 @@ def retrieve_data(api_url, base_file_name):
 
 # add python main file check
 if __name__ == "__main__":
+    # Define the API URLs to retrieve data from
     file_names = [
         "Afstemning",
         "Afstemningstype",
